08c757b6e2c1f0b071bb3fa72533b105d9b89fed
JulianDuque97 <julian.a97@hotmail.es>
2022-05-19 00:15:23 -0500

    agregando funcionabilidades

--- main.py
+++ main.py
@@ -3,25 +3,53 @@ from random import randint
 
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-from PyQt5 import uic, QtWidgets, QtCore, QtGui
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 
-qtcreator_file  = "UI.ui" # Enter file here.
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
-
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+import logos_rc
 #-----------------------------------------------------------------------------------------------------------------#
 #Thread
 
 #-----------------------------------------------------------------------------------------------------------------#
 #GUI
-class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
-        self.setupUi(self)
+class Datos_paciente(QMainWindow):
+    def __init__(self, parent=None):
+        super(Datos_paciente, self).__init__(parent)
+        loadUi('Datos_paciente.ui', self)
+        self.altura_linedit.editingFinished.connect(self.datos)
+        self.peso_linedit.editingFinished.connect(self.datos)
+        self.sexo_linedit.editingFinished.connect(self.datos)
+        self.covid_linedit.editingFinished.connect(self.datos)
+        self.siguiente_button.clicked.connect(self.siguiente)
+        
+    def datos(self):
+        self.altura = self.altura_linedit.text()
+        self.peso = self.peso_linedit.text()
+        self.sexo = self.sexo_linedit.text()
+        self.covid = self.covid_linedit.text()
+        
+    def siguiente(self):
+        self.hide()
+        previous_window = Pop_up(self)
+        previous_window.show()	
+        
+class Pop_up(QMainWindow):
+    def __init__(self, parent=None):
+        super(Pop_up, self).__init__(parent)
+        loadUi('pop_up.ui', self)
+        self.ok_button.clicked.connect(self.siguiente)
+        
+    def siguiente(self):
+        self.hide()
+        previous_window = MyApp(self)
+        previous_window.show()
+
+class MyApp(QMainWindow):
+    def __init__(self, parent=None):
+        super(MyApp, self).__init__(parent)
+        loadUi('main.ui', self)
                 
         self.on.setEnabled(True)
         self.off.setEnabled(False)
@@ -30,33 +58,45 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.systems.toggle()
         self.on.clicked.connect(self.btn_on)
         self.off.clicked.connect(self.btn_off)
-                        
+        self.datos_button.clicked.connect(self.datos_paciente)
+                                
         self.x = list(range(100))  # 100 time points
-        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
         
+        self.y1 = [randint(0,100) for _ in range(100)]  # 100 data points
         self.y2 = [randint(0,100) for _ in range(100)]  # 100 data points
+        self.y3 = [randint(0,100) for _ in range(100)]  # 100 data points
 
         self.graphWidget_1.setBackground('w')    
-        self.graphWidget_2.setBackground('w')    
-                    
+        self.graphWidget_2.setBackground('w')
+        self.graphWidget_3.setBackground('w')    
+    
+    def datos_paciente(self):
+        self.hide()
+        next_window = Datos_paciente(self)
+        next_window.show()                       
+    
     def update_plot_data(self):
         self.x = self.x[1:]  # Remove the first y element.
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-        self.y = self.y[1:]  # Remove the first
-        self.y.append(randint(0,100))  # Add a new random value.
         
+        self.y1 = self.y1[1:]  # Remove the first
+        self.y1.append(randint(0,100))  # Add a new random value.
         self.y2 = self.y2[1:]  # Remove the first
         self.y2.append(randint(0,200))  # Add a new random value.
+        self.y3 = self.y3[1:]  # Remove the first
+        self.y3.append(randint(0,200))  # Add a new random value.
         
-        self.Ppeak.setText(str(self.y[-1]))
-        self.label_29.setText(str(self.y[-1]))
-        self.label_32.setText(str(self.y[-1]))
-        self.label_35.setText(str(self.y[-1]))
+        self.Ppeak_label.setText(str(self.y1[-1]))
+        self.label_29.setText(str(self.y1[-1]))
+        self.label_32.setText(str(self.y1[-1]))
+        self.label_35.setText(str(self.y1[-1]))
         
-        self.data_line_1.setData(self.x, self.y)  # Update the data.    
-        self.data_line_2.setData(self.x, self.y2)  # Update the data.    
+        self.data_line_1.setData(self.x, self.y1)  # Update the data.    
+        self.data_line_2.setData(self.x, self.y2)  # Update the data. 
+        self.data_line_3.setData(self.x, self.y3)  # Update the data.    
         
     def btn_on(self):
+        
         self.img_on_off.setPixmap(QtGui.QPixmap("Images/oxygen-mask.jpg"))
         self.lb_on_off.setText("System: On")
         self.on.setEnabled(False)
@@ -64,8 +104,10 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         
         pen = pg.mkPen(color=(255, 0, 0))
         pen2 = pg.mkPen(color=(0, 0, 255))
-        self.data_line_1 =  self.graphWidget_1.plot(self.x, self.y, pen=pen)        
+        pen3 = pg.mkPen(color=(0, 128, 0))
+        self.data_line_1 =  self.graphWidget_1.plot(self.x, self.y1, pen=pen)        
         self.data_line_2 =  self.graphWidget_2.plot(self.x, self.y2, pen=pen2)
+        self.data_line_3 =  self.graphWidget_3.plot(self.x, self.y2, pen=pen3)
         
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
@@ -80,15 +122,13 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         
         self.graphWidget_1.clear()
         self.graphWidget_2.clear()
+        self.graphWidget_3.clear()
         self.timer.stop()     
         
-#-----------------------------------------------------------------------------------------------------------------#
-#This is the function that works as Thread    
-
-    
+	
 #-----------------------------------------------------------------------------------------------------------------#
 #-----------------------------------------------------------------------------------------------------------------#
-app = QtWidgets.QApplication(sys.argv)
-window = MyApp()
-window.show()
+app = QApplication(sys.argv)
+main = Datos_paciente()
+main.show()
 sys.exit(app.exec_())
